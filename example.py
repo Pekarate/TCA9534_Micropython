469d14bd67e7d8ae49a1481a9d73fbf9fc9d5038
AKA <nfeehan@gmail.com>
2020-09-05 17:27:50 -0400

    added convenience method

--- example.py
+++ example.py
@@ -1,7 +1,7 @@
 import tca9534
 import time
 
-dev = tca9534.TCA9534(output=True) # initialize all pins to be outputs
+dev = tca9534.TCA9534(output=True)  # initialize all pins to be outputs
 # dev = tca9534.TCA9534(bitmask=0b01110011) # initialize w/ specific inputs and outputs enabled
 
 while True:
